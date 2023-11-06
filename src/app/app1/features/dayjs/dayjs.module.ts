daed6b2f16fb83d1ea44fb956ac5094cb4756921
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-11-06 20:10:58 +0700

    chore: npx ng g m app1/features/dayjs --routing

---
+++ src/app/app1/features/dayjs/dayjs.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DayjsRoutingModule } from './dayjs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DayjsRoutingModule
+  ]
+})
+export class DayjsModule { }
