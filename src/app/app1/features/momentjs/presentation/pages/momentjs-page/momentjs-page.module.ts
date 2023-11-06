c8cac896eff978a65cc6c5d5e99dbaf81996c7e6
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-11-06 20:04:01 +0700

    chore: npx ng g c app1/features/momentjs/presentation/pages/momentjsPage --routing

---
+++ src/app/app1/features/momentjs/presentation/pages/momentjs-page/momentjs-page.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MomentjsPageRoutingModule } from './momentjs-page-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MomentjsPageRoutingModule
+  ]
+})
+export class MomentjsPageModule { }
