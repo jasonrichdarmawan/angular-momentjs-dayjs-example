4f60ee835d94ec03d5e4907350d2775e8cdd1f67
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-11-06 19:48:53 +0700

    chore: npx ng g m app1 --routing

---
+++ src/app/app1/app1.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { App1RoutingModule } from './app1-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    App1RoutingModule
+  ]
+})
+export class App1Module { }
