4596797782203c8028890ede59c2a4a69f37e069
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-11-06 20:08:25 +0700

    chore: npx ng g m app1/features/momentjs --routing

---
+++ src/app/app1/features/momentjs/momentjs.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MomentjsRoutingModule } from './momentjs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MomentjsRoutingModule
+  ]
+})
+export class MomentjsModule { }
